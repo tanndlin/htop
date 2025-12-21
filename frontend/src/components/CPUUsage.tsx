11b2325f131c3d3955faeda71a8aae04f4001b7e
tanndlin <tanndlin@gmail.com>
2025-12-21 18:33:22 -0500

    Move loading text

--- frontend/src/components/CPUUsage.tsx
+++ frontend/src/components/CPUUsage.tsx
@@ -17,33 +17,35 @@ const CPUUsage = (props: Props) => {
         return () => clearInterval(interval);
     }, [refreshRate, refetch]);
 
+    return (
+        <div className="flex flex-col gap-2 pb-4 border-2 rounded-md w-max border-secondary">
+            <header className="flex justify-between p-2 px-4 rounded-t-md bg-secondary">
+                <h1>CPU Usages</h1>
+            </header>
+            <div className="flex px-4">{getContent(loading, error, data)}</div>
+        </div>
+    );
+};
+
+function getContent(loading: boolean, error: string, data: CpuInfo | null) {
     if (loading) {
         return <div>Loading...</div>;
     }
     if (error) {
         return <div>{error}</div>;
     }
-
     if (!data) {
         return <div>No data available</div>;
     }
-
     return (
-        <div className="flex flex-col gap-2 pb-4 border-2 rounded-md w-max border-secondary">
-            <header className="flex justify-between p-2 px-4 rounded-t-md bg-secondary">
-                <h1>CPU Usages</h1>
-            </header>
-            <div className="flex px-4">
-                <ul className="grid grid-cols-6 gap-4">
-                    {data.cpu_usages.map((usage, index) => (
-                        <div className="w-24 h-24" key={index}>
-                            <CPU id={index + 1} usage={usage} />
-                        </div>
-                    ))}
-                </ul>
-            </div>
-        </div>
+        <ul className="grid grid-cols-6 gap-4">
+            {data.cpu_usages.map((usage, index) => (
+                <div className="w-24 h-24" key={index}>
+                    <CPU id={index + 1} usage={usage} />
+                </div>
+            ))}
+        </ul>
     );
-};
+}
 
 export default CPUUsage;
--- frontend/src/components/RAMUsage.tsx
+++ frontend/src/components/RAMUsage.tsx
@@ -17,32 +17,40 @@ const RAMUsage = (props: Props) => {
         return () => clearInterval(interval);
     }, [refreshRate, refetch]);
 
+    return (
+        <div className="flex flex-col w-full gap-2 pb-4 border-2 rounded-md border-secondary">
+            <header className="flex justify-between p-2 px-4 rounded-t-md bg-secondary">
+                <h1>RAM Usage</h1>
+                <span className="flex gap-1">
+                    <p>
+                        Used{' '}
+                        {((data?.used ?? 0) / Math.pow(1024, 3)).toFixed(2)} GB
+                    </p>
+                    <p>/</p>
+                    <p>
+                        {((data?.total ?? 0) / Math.pow(1024, 3)).toFixed(2)} GB
+                    </p>
+                </span>
+            </header>
+            <div className="flex h-full px-4">
+                {getContent(loading, error, data)}
+            </div>
+        </div>
+    );
+};
+
+function getContent(loading: boolean, error: string, data: RamInfo | null) {
     if (loading) {
         return <div>Loading...</div>;
     }
     if (error) {
         return <div>{error}</div>;
     }
-
     if (!data) {
         return <div>No data available</div>;
     }
 
-    return (
-        <div className="flex flex-col w-full gap-2 pb-4 border-2 rounded-md border-secondary">
-            <header className="flex justify-between p-2 px-4 rounded-t-md bg-secondary">
-                <h1>RAM Usage</h1>
-                <span className="flex gap-1">
-                    <p>Used {(data.used / Math.pow(1024, 3)).toFixed(2)} GB</p>
-                    <p>/</p>
-                    <p>{(data.total / Math.pow(1024, 3)).toFixed(2)} GB</p>
-                </span>
-            </header>
-            <div className="flex h-full px-4">
-                <CPU usage={(data.used / data.total) * 100} />
-            </div>
-        </div>
-    );
-};
+    return <CPU usage={(data.used / data.total) * 100} />;
+}
 
 export default RAMUsage;
