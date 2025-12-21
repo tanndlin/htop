12c18025785400c3dc620723e037ccb93c7609af
tanndlin <tanndlin@gmail.com>
2025-12-21 13:06:35 -0500

    Inital commit

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+use axum::{Router, routing::get};
+
+#[tokio::main]
+async fn main() {
+    // build our application with a single route
+    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+
+    // run our app with hyper, listening globally on port 3000
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:7032")
+        .await
+        .unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
